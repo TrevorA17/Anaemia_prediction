9d271ad6381840e4114d9ae6a459b798fc657bee
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-04-29 12:48:11 +0300

    Update ExploratoryDataAnalysis.R

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -15,3 +15,53 @@ summary(AnaemiaData)
 
 #View dataset
 View(AnaemiaData)
+
+# Measures of Frequency
+# Frequency of the target variable
+table(AnaemiaData$Anaemic)
+
+# Frequency of Hb ranges (e.g. grouping Hb levels)
+cut_Hb <- cut(AnaemiaData$Hb, breaks = c(0, 7, 10, 13, 16, Inf), right = FALSE)
+table(cut_Hb)
+
+# Measures of Central Tendency
+# Mean
+mean(AnaemiaData$Red_Pixel)
+mean(AnaemiaData$Green_pixel)
+mean(AnaemiaData$Blue_pixel)
+mean(AnaemiaData$Hb)
+
+# Median
+median(AnaemiaData$Red_Pixel)
+median(AnaemiaData$Green_pixel)
+median(AnaemiaData$Blue_pixel)
+median(AnaemiaData$Hb)
+
+# Mode function
+get_mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+get_mode(AnaemiaData$Anaemic)
+
+# Measures of Distribution
+# Range
+range(AnaemiaData$Hb)
+
+# Variance and standard deviation
+var(AnaemiaData$Hb)
+sd(AnaemiaData$Hb)
+
+# Skewness and kurtosis (requires e1071)
+library(e1071)
+skewness(AnaemiaData$Hb)
+kurtosis(AnaemiaData$Hb)
+
+# Measures of Relationship
+# Correlation matrix among pixel values and Hb
+cor(AnaemiaData[, c("Red_Pixel", "Green_pixel", "Blue_pixel", "Hb")])
+
+# Chi-square test between Anaemic and binned Hb
+Hb_group <- cut(AnaemiaData$Hb, breaks = c(0, 10, 12, Inf), labels = c("Low", "Medium", "High"))
+chisq.test(table(Hb_group, AnaemiaData$Anaemic))
